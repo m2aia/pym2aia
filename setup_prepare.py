cf2b918c9f5a69d0bf8b0cea4c837ef761a1c43d
Jonas Cordes <j.cordes@hs-mannheim.de>
2022-12-05 12:04:39 +0100

    prepare build by enter os

--- setup_prepare.py
+++ setup_prepare.py
@@ -3,31 +3,33 @@ import pathlib
 import zipfile
 import tarfile
 import shutil
+import sys
 
-linux_archive = pathlib.Path("install_binaries/linux.tar.gz")
-linux_extracted = pathlib.Path("install_binaries/linux")
-
-windows_archive = pathlib.Path("install_binaries/windows.zip")
-windows_extracted = pathlib.Path("install_binaries/windows")
-
-
-if not linux_archive.exists():
-    wget.download("https://data.jtfc.de/latest/ubuntu20_04/M2aia-latest.tar.gz", str(linux_archive))
-    with tarfile.open(str(linux_archive)) as f:
-        f.extractall(str(linux_extracted))
-
-if not windows_archive.exists():
-    wget.download("https://data.jtfc.de/latest/windows/M2aia-2022.10.00-windows-x86_64.zip", str(windows_archive))
-    with zipfile.ZipFile(str(windows_archive)) as f:
-        f.extractall(str(windows_extracted))
-
-
-
+pathlib.Path("install_binaries").mkdir(exist_ok=True)
 binaries_root=pathlib.Path("src/m2aia/binaries")
 binaries_root.mkdir(exist_ok=True)
 
-windows_root=list(windows_extracted.glob("*windows*"))[0]
-# linux_root=list(linux_extracted.glob("*linux*"))[0]
 
-# shutil.copytree(linux_root, binaries_root, dirs_exist_ok=True)
-shutil.copytree(windows_root, binaries_root, dirs_exist_ok=True)
+if "linux" in sys.argv[1]:
+    linux_archive = pathlib.Path("install_binaries/linux.tar.gz")
+    linux_extracted = pathlib.Path("install_binaries/linux")
+    if not linux_archive.exists():
+        wget.download("https://data.jtfc.de/latest/ubuntu20_04/M2aia-latest.tar.gz", str(linux_archive))
+        with tarfile.open(str(linux_archive)) as f:
+            f.extractall(str(linux_extracted))
+    linux_root=list(linux_extracted.glob("*linux*"))[0]
+    shutil.copytree(linux_root, binaries_root, dirs_exist_ok=True)
+
+
+if "win" in sys.argv[1]:
+    windows_archive = pathlib.Path("install_binaries/windows.zip")
+    windows_extracted = pathlib.Path("install_binaries/windows")
+    if not windows_archive.exists():
+        wget.download("https://data.jtfc.de/latest/windows/M2aia-2022.10.00-windows-x86_64.zip", str(windows_archive))
+        with zipfile.ZipFile(str(windows_archive)) as f:
+            f.extractall(str(windows_extracted))
+    windows_root=list(windows_extracted.glob("*windows*"))[0]
+    shutil.copytree(windows_root, binaries_root, dirs_exist_ok=True)
+
+
+
