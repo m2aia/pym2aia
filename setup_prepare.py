751c12a3c092c494f43f707be5da69d07761a335
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-08-30 00:17:16 +0200

    more debug logs

--- setup_prepare.py
+++ setup_prepare.py
@@ -19,15 +19,22 @@ try:
         wget.download(f"https://data.jtfc.de/latest/linux/M2aia-{os.environ['M2AIA_VERSION']}.tar.gz", str(linux_archive))
         with tarfile.open(str(linux_archive)) as f:
             f.extractall(str(linux_extracted))
+        print("linux_extracted:", linux_extracted)
 
     # find libraries
     linux_root=list(linux_extracted.glob("*linux*"))[0]
+    print("linux_root:", linux_root)
     os.environ["M2AIA_PATH"] = str(linux_root.joinpath("bin").absolute())
+    print("M2AIA_PATH:", os.environ["M2AIA_PATH"])
+
+    # init module
     import m2aia
     invoked_libraries = os.environ["M2AIA_LIBRARIES"]
+    print("invoked_libraries:", invoked_libraries)
 
     linux_bin_root = binaries_root.joinpath('linux')
     linux_bin_root.mkdir(exist_ok=True)
+    print("linux_bin_root:", linux_bin_root)
 
     for d in invoked_libraries.split(';'):
         d = pathlib.Path(d)
