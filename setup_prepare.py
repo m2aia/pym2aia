4e42c2e902b3563b3361229fb962aaf19f1494bc
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-08-30 00:05:48 +0200

    add exception info to log

--- setup_prepare.py
+++ setup_prepare.py
@@ -33,8 +33,8 @@ try:
         d = pathlib.Path(d)
         print(str(d), "->", str(linux_bin_root.joinpath(d.name)))
         shutil.copy(str(d),str(linux_bin_root.joinpath(d.name)))
-except:
-    print("Linux binaries not found!")
+except Exception as e:
+    print("Linux binaries not found!", e)
 
 # START WINDOWS
 print("=================================================")
@@ -59,5 +59,5 @@ try:
             continue
         print(str(lib), "->", str(windows_bin_root.joinpath(lib.name)))
         shutil.copy(str(lib),str(windows_bin_root.joinpath(lib.name)))
-except:
-    print("Windows binaries not found!")
+except Exception as e:
+    print("Windows binaries not found!", e)
