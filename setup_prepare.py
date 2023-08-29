9d4c97f92e54d1830e4e5eeb3f12b6ead9706404
Jonas Cordes <j.cordes@hs-mannheim.de>
2023-08-29 13:14:00 +0200

    setup preparation now uses version tag

--- setup_prepare.py
+++ setup_prepare.py
@@ -11,46 +11,51 @@ pathlib.Path("install_binaries").mkdir(exist_ok=True)
 binaries_root=pathlib.Path("src/m2aia/bin/")
 shutil.rmtree(binaries_root, ignore_errors=True)
 binaries_root.mkdir(exist_ok=True, parents=True)
+try:
+    linux_archive = pathlib.Path("install_binaries/linux.tar.gz")
+    linux_extracted = pathlib.Path("install_binaries/linux")
+    if not linux_archive.exists():
+        wget.download(f"https://data.jtfc.de/latest/linux/M2aia-{os.environ['M2AIA_VERSION']}.tar.gz", str(linux_archive))
+        with tarfile.open(str(linux_archive)) as f:
+            f.extractall(str(linux_extracted))
 
-linux_archive = pathlib.Path("install_binaries/linux.tar.gz")
-linux_extracted = pathlib.Path("install_binaries/linux")
-if not linux_archive.exists():
-    wget.download("https://data.jtfc.de/latest/ubuntu20_04/M2aia-latest.tar.gz", str(linux_archive))
-    with tarfile.open(str(linux_archive)) as f:
-        f.extractall(str(linux_extracted))
+    # find libraries
+    linux_root=list(linux_extracted.glob("*linux*"))[0]
+    os.environ["M2AIA_PATH"] = str(linux_root.joinpath("bin").absolute())
+    import m2aia
+    invoked_libraries = os.environ["M2AIA_LIBRARIES"]
 
-# find libraries
-linux_root=list(linux_extracted.glob("*linux*"))[0]
-os.environ["M2AIA_PATH"] = str(linux_root.joinpath("bin").absolute())
-import m2aia
-invoked_libraries = os.environ["M2AIA_LIBRARIES"]
+    linux_bin_root = binaries_root.joinpath('linux')
+    linux_bin_root.mkdir(exist_ok=True)
 
-linux_bin_root = binaries_root.joinpath('linux')
-linux_bin_root.mkdir(exist_ok=True)
-
-for d in invoked_libraries.split(';'):
-    d = pathlib.Path(d)
-    print(str(d), "->", str(linux_bin_root.joinpath(d.name)))
-    shutil.copy(str(d),str(linux_bin_root.joinpath(d.name)))
+    for d in invoked_libraries.split(';'):
+        d = pathlib.Path(d)
+        print(str(d), "->", str(linux_bin_root.joinpath(d.name)))
+        shutil.copy(str(d),str(linux_bin_root.joinpath(d.name)))
+except:
+    print("Linux binaries not found!")
 
 # START WINDOWS
 print("=================================================")
-windows_archive = pathlib.Path("install_binaries/windows.zip")
-windows_extracted = pathlib.Path("install_binaries/windows")
-if not windows_archive.exists():
-    wget.download("https://data.jtfc.de/latest/windows/M2aia-2022.10.00-windows-x86_64.zip", str(windows_archive))
-    with zipfile.ZipFile(str(windows_archive)) as f:
-        f.extractall(str(windows_extracted))
-windows_root=list(windows_extracted.glob("*windows*"))[0]
+try:
+    windows_archive = pathlib.Path("install_binaries/windows.zip")
+    windows_extracted = pathlib.Path("install_binaries/windows")
+    if not windows_archive.exists():
+        wget.download(f"https://data.jtfc.de/latest/windows/M2aia-{os.environ['M2AIA_VERSION']}.zip", str(windows_archive))
+        with zipfile.ZipFile(str(windows_archive)) as f:
+            f.extractall(str(windows_extracted))
+    windows_root=list(windows_extracted.glob("*windows*"))[0]
 
-windows_bin_root = binaries_root.joinpath('windows')
-windows_bin_root.mkdir(exist_ok=True)    
+    windows_bin_root = binaries_root.joinpath('windows')
+    windows_bin_root.mkdir(exist_ok=True)    
 
-globs = [f for f in windows_root.joinpath('bin').glob("*.dll")]
-globs.extend([f for f in windows_root.joinpath('bin').glob("MitkCore/*.dll")])
-for lib in globs:
-    if "Qt5" in str(lib):
-        print("FOUND => ", lib)
-        continue
-    print(str(lib), "->", str(windows_bin_root.joinpath(lib.name)))
-    shutil.copy(str(lib),str(windows_bin_root.joinpath(lib.name)))
+    globs = [f for f in windows_root.joinpath('bin').glob("*.dll")]
+    globs.extend([f for f in windows_root.joinpath('bin').glob("MitkCore/*.dll")])
+    for lib in globs:
+        if "Qt5" in str(lib):
+            print("FOUND => ", lib)
+            continue
+        print(str(lib), "->", str(windows_bin_root.joinpath(lib.name)))
+        shutil.copy(str(lib),str(windows_bin_root.joinpath(lib.name)))
+except:
+    print("Windows binaries not found!")
